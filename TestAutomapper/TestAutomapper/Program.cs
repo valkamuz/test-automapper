98a47d5b37cf6b0001863cd6c0a08e361ee415d5
valkamuz <valkamuz@yandex.ru>
2019-05-19 23:27:06 +0500

    Реализован простейший вариант для демонстрации возможностей автомапера. 	new file:   .gitattributes 	new file:   .gitignore 	new file:   TestAutomapper/TestAutomapper.sln 	new file:   TestAutomapper/TestAutomapper/App.config 	new file:   TestAutomapper/TestAutomapper/Classes/AuthorDTO.cs 	new file:   TestAutomapper/TestAutomapper/Classes/AuthorModel.cs 	new file:   TestAutomapper/TestAutomapper/Program.cs 	new file:   TestAutomapper/TestAutomapper/Properties/AssemblyInfo.cs 	new file:   TestAutomapper/TestAutomapper/TestAutomapper.csproj 	new file:   TestAutomapper/TestAutomapper/packages.config

---
+++ TestAutomapper/TestAutomapper/Program.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAutomapper.Classes;
+
+namespace TestAutomapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<AuthorModel, AuthorDTO>(); });
+
+            IMapper iMapper = config.CreateMapper();
+            AuthorModel source = new AuthorModel
+            { 
+                Id          = 1,
+                FirstName   = "Иван",
+                LastName    = "Иванович",
+                Address     = "Тюмень"
+            };
+            AuthorDTO destination = iMapper.Map<AuthorModel, AuthorDTO>(source);
+            Console.WriteLine(destination.FirstName);
+        }
+    }
+}
